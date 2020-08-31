f9de04710b87adf22ce4169fcb044df89cceaa60
Luciano Lorenzo <lorenzoluciano@gmail.com>
2020-08-31 01:25:16 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,8 @@
 while true; do
 
 	php /var/www/html/PostingTwitterApp/video2frame/video2frame.php
-    #sleep 4m
-    php /var/www/html/PostingTwitterApp/frame2twitter/frame2twitter.php
-    sleep 10m
+	php /var/www/html/PostingTwitterApp/frame2twitter/frame2twitter.php
+	sleep 10m
 
 done
 
